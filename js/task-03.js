b31b74c136dee769c41ba28660568fc1c96ee2e4
NovicovMykhailo <mikenovicov@gmail.com>
2023-02-20 17:14:06 +0200

    add 4th task

--- js/task-03.js
+++ js/task-03.js
@@ -1,17 +1,3 @@
-const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
-];
 /*
 Напиши скрипт для создания галереи изображений по массиву данных. 
 В HTML есть список ul.gallery.
@@ -39,3 +25,33 @@ const images = [
   },
 ];
 */
+const images = [
+	{
+		url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "White and Black Long Fur Cat",
+	},
+	{
+		url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+	},
+	{
+		url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "Group of Horses Running",
+	},
+];
+
+
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryList(array) {
+  let listItems =[];
+  for( let el of array){
+    listItems.push(`<li class = gallery__item><img class="gallery__img" src='${el.url}' alt=${el.alt} ></li>`)
+  }
+  return listItems
+}
+
+const galleryItems = createGalleryList(images);
+console.log()
+
+galleryList.insertAdjacentHTML("afterbegin", galleryItems.slice(',').join(''))
