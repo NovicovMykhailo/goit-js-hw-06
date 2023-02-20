ee1a4a0542a096cf8a6f4c8aa15f7ceccc4f7e42
NovicovMykhailo <mikenovicov@gmail.com>
2023-02-20 19:21:56 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -12,12 +12,46 @@
   <button type="submit">Login</button>
 </form>
 
-Обработка отправки формы form.login-form должна быть по событию submit.
-При отправке формы страница не должна перезагружаться.
-Если в форме есть незаполненные поля, выводи alert 
+1 Обработка отправки формы form.login-form должна быть по событию submit.
+
+2 При отправке формы страница не должна перезагружаться.
+
+3 Если в форме есть незаполненные поля, выводи alert 
   с предупреждением о том, что все поля должны быть заполнены.
-Если пользователь заполнил все поля и отправил форму, 
-  собери значения полей в обьект, где имя поля будет именем свойства, 
-   а значение поля - значением свойства. 
+
+4 Если пользователь заполнил все поля и отправил форму, 
+  - собери значения полей в обьект, 
+    где имя поля будет именем свойства,  а значение поля - значением свойства. 
     Для доступа к элементам формы используй свойство elements.
-Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset. */
+
+5 Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset. */
+
+const refs = {
+	form: document.querySelector(".login-form"),
+};
+
+refs.form.addEventListener("submit", onSubmit);
+
+
+
+function onSubmit(event) {
+	event.preventDefault();
+
+	const formElements = event.currentTarget.elements;
+	const email = formElements.email.value;
+	const password = formElements.password.value;
+
+	if (email === '' || password === '') {
+		window.alert("все поля должны быть заполнены");
+	}
+
+	const formData = {
+		email,
+		password,
+	};
+
+  console.table(formData)
+  
+  refs.form.reset()
+	
+}
