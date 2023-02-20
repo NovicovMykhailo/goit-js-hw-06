2a9d2bf8bd08c18bc599bbe019d8cd9551283ea2
NovicovMykhailo <mikenovicov@gmail.com>
2023-02-20 23:49:17 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -32,7 +32,12 @@ refs.input.addEventListener("blur", onInputBlur);
 
 function onInputBlur(event) {
 	let inputLength = event.currentTarget.value.length;
-	inputLength >= refs.length
-		? (refs.input.classList.add("valid"), refs.input.classList.remove("invalid"))
-		: (refs.input.classList.add("invalid"), refs.input.classList.remove("valid"));
+	console.log(inputLength);
+	if (inputLength === Number(refs.length)) {
+		refs.input.classList.add("valid");
+		refs.input.classList.remove("invalid");
+	} else {
+		refs.input.classList.add("invalid");
+		refs.input.classList.remove("valid");
+	}
 }
